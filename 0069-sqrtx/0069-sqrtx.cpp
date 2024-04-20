496958df0aa96e2e8e58ef42a8d189ba637d78bb
Mohit Mittal <95767369+NotMohitMittal@users.noreply.github.com>
2024-04-21 01:50:18 +0530

    Update 0069-sqrtx.cpp

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,3 +1,4 @@
+// less efficient and more time consuming;
 class Solution {
 public:
     int mySqrt(int x) {
@@ -10,5 +11,23 @@ public:
             }
         }
         return --count;
+    }
+};
+
+
+// more better algo for the same is: 
+class Solution {
+public:
+    long int mySqrt(int x) {
+        long int start = 0;
+        long int end = x;
+
+        while (start <= end){
+            long int mid = start + (end-start)/2;
+            if(mid*mid == x) return mid;
+            if(x < mid*mid) end = mid-1;
+            if(x > mid*mid) start = mid+1;
+        }
+        return end;
     }
 };
